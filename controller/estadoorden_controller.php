96d0e4b469c6d2f3e2e1bd4b35d02e74ff8b71eb
Gilson Ponce Briones <gjponce@espol.edu.ec>
2020-11-12 11:34:32 -0500

    estado de orden hecha y modelos

---
+++ controller/estadoorden_controller.php
@@ -0,0 +1,71 @@
+<?php
+class ControladorEstadoOrden
+{
+
+
+    public function index()
+    {
+        $usuario = ModeloUsuario::index("usuario");
+        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            foreach ($usuario as $key => $valueUsuario) {
+                if (
+                    "Basic " . base64_encode($_SERVER['PHP_AUTH_USER'] . ":" . $_SERVER['PHP_AUTH_PW']) ==
+                    "Basic " . base64_encode($valueUsuario["llave"] . ":" . $valueUsuario["codigo"])
+                ) {
+                    $orden = ModeloEstadoOrden::index("estado_orden");
+                    $json = array(
+                        "status" => 200,
+                        "total_registro" => count($orden),
+                        "detalle" => $orden
+                    );
+                    echo json_encode($json, true);
+                }
+            }
+        }
+    }
+
+    public function create($datos)
+    {
+
+        if (isset($datos['nombre']) && !preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/', $datos["nombre"])) {
+            $json = array(
+
+                "status" => 404,
+                "detalle" => "Error"
+            );
+
+            echo json_encode($json, true);
+
+            return;
+        }
+
+        $datos = array(
+            "nombre" => $datos['nombre']
+        );
+
+
+        $usuario = ModeloUsuario::index("usuario");
+        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            foreach ($usuario as $key => $valueUsuario) {
+                if (
+                    "Basic " . base64_encode($_SERVER['PHP_AUTH_USER'] . ":" . $_SERVER['PHP_AUTH_PW']) ==
+                    "Basic " . base64_encode($valueUsuario["llave"] . ":" . $valueUsuario["codigo"])
+                ) {
+                    $create = ModeloEstadoOrden::create("estado_orden", $datos);
+
+                    if ($create == 'ok') {
+                        $json = array(
+
+                            "status" => 200,
+                            "detalle" => "Registro exitoso de orden"
+                        );
+
+                        echo json_encode($json, true);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
